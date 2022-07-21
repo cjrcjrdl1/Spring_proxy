1018d051c43cf4d63ddb75895ee0eb276d9d05b5
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-07-21 16:11:48 +0900

    예제 프로젝트 만들기 v3

--- proxy/src/main/java/hello/proxy/app/v2/OrderServiceV2.java
+++ proxy/src/main/java/hello/proxy/app/v2/OrderServiceV2.java
@@ -3,7 +3,7 @@ package hello.proxy.app.v2;
 import hello.proxy.app.v1.OrderRepositoryV1;
 import hello.proxy.app.v1.OrderServiceV1;
 
-public class OrderServiceV2 implements OrderServiceV1 {
+public class OrderServiceV2 {
 
     private final OrderRepositoryV2 orderRepository;
 
@@ -11,7 +11,6 @@ public class OrderServiceV2 implements OrderServiceV1 {
         this.orderRepository = orderRepository;
     }
 
-    @Override
     public void orderItem(String itemId) {
         orderRepository.save(itemId);
     }
